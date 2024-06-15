8208abb93e1f7d26d6c7734a318dd3db0b676444
DevCaioSouza <caiocbahia@hotmail.com>
2024-06-15 20:35:22 -0300

    Create User resource following DDD rules - folder structure added

---
+++ src/users/domain/entities/user.entity.ts
@@ -0,0 +1,4 @@
+export class UserEntity {}
+
+//obs: A entidade é um recurso específico da camada de domínio. É principal recurso da aplicação
+// Praticamente, ela é em si o "motivo da existência do software"
