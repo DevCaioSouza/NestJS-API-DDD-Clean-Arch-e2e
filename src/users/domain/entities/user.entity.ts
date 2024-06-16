aef1a6259e1f862c0981680a9e49d8b2d117fb61
DevCaioSouza <caiocbahia@hotmail.com>
2024-06-16 11:06:05 -0300

    Created User Entity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,4 +1,18 @@
-export class UserEntity {}
+export type UserProps = {
+  name: string
+  email: string
+  password: string
+  createdAt: Date
+}
+
+// DDD: Qualquer propriedade da entidade só pode ser manipulada pela própria entidade
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date();
+    // "??" Operador de coalescência nula
+  }
+}
 
 //obs: A entidade é um recurso específico da camada de domínio. É principal recurso da aplicação
 // Praticamente, ela é em si o "motivo da existência do software"
+
