2d2bf75ca09b0fd8eae8fa5bc2cfe71349929547
DevCaioSouza <caiocbahia@hotmail.com>
2024-06-20 22:51:49 -0300

    Add update Methods

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -14,9 +14,22 @@ export class UserEntity extends Entity<UserProps> {
     this.props.createdAt = this.props.createdAt ?? new Date();
   }
 
+  update(value: string): void {
+    this.name = value
+  }
+
+  updatePassword(value: string): void {
+    this.password = value
+  }
+
   get name() {
     return this.props.name
   }
+
+  private set name(value: string) {
+    this.props.name = value
+  }
+
 
   get email() {
     return this.props.email
@@ -25,6 +38,11 @@ export class UserEntity extends Entity<UserProps> {
   get password() {
     return this.props.password
   }
+
+  private set password(value: string) {
+    this.props.password = value
+  }
+
 
   get createdAt() {
     return this.props.createdAt
